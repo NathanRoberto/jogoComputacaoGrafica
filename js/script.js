e38ba423f33bbaa69ed9dfecb387bc6b5eacb060
Nathan Roberto <nathanroberto38@gmail.com>
2025-06-12 17:48:46 -0300

    Melhorando a estrutura do codigo

---
+++ js/script.js
@@ -0,0 +1,51 @@
+  $(document).ready(() => {
+
+        setTimeout(function () {
+            $(".modal").css("display", "block")
+        }, 1000)
+
+
+        $("#btn_iniciar").on("click", () => {
+            $(".modal").css("display", "none")
+            $("#main").show()
+            $("#texto").html('Para reinciar recarregue a página')
+
+        })
+
+        const mario = document.querySelector('.mario')
+        const pipe = document.querySelector('.pipe')
+
+        const jump = () => {
+            mario.classList.add('jump')
+
+            setTimeout(() => {
+                mario.classList.remove("jump")
+            }, 500)
+        }
+
+        const loop = setInterval(() => {
+
+            const pipePosition = pipe.offsetLeft;
+            const marioPosition = +window.getComputedStyle(mario).bottom.replace('px', '');
+
+            if (pipePosition <= 120 && pipePosition > 0 && marioPosition < 80) {
+
+                pipe.style.animation = 'none';
+                pipe.style.left = `${pipePosition}px`;
+
+                mario.style.animation = 'none';
+                mario.style.left = `${marioPosition}px`;
+
+                mario.src = 'images/game-over.png'
+                mario.style.width = '75px'
+                mario.style.marginLeft = '50px'
+
+                clearInterval(loop)
+
+            }
+
+        }, 10)
+
+        document.addEventListener('keydown', jump)
+
+    })
